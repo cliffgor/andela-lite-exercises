4867064dd146eee71b0a1f4248e46c31c55fef75
cliffgor <clierozigy@gmail.com>
2016-07-16 14:35:40 +0300

    added students name nothing extra

--- students.py
+++ students.py
@@ -1,3 +1,7 @@
-students.py
-sfjhadfsk
-ihiuhig
+name=["cliff","gor"]
+students={"name":name,"dateofbirth":19202015}
+firstname=students.get(name)
+print=firstname
+lastname=students.get(name)
+print=lastname
+
